9972e7cf272327ff75c92b666e66be23c48aeeb2
Narendiran2004 <narendirans.22cse@kongu.edu>
2023-02-26 20:02:25 +0530

    abc

---
+++ yes.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	for(i=0;i<10000;i++)
+	{
+		printf("I LOVE YOU   I LOVE YOU   I LOVE YOU   I LOVE YOU   I LOVE YOU   I LOVE YOU   I LOVE YOU   I LOVE YOU\n");
+	} 
+	return 0;
+}
